41e9ec604fdcab47b71697d6ed532b3703c671d5
ishimori <ishimori_yuzuru@probsc.jp>
2017-08-18 11:53:58 +0900

    Carousel

--- www/js/test1.js
+++ www/js/test1.js
@@ -1,11 +1,15 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
+var prev = function() {
+  var carousel = document.getElementById('carousel');
+  carousel.prev();
+};
 
-  if (page.id === 'page1') {
-    page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
-    };
-  } else if (page.id === 'page2') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
+var next = function() {
+  var carousel = document.getElementById('carousel');
+  carousel.next();
+};
+
+ons.ready(function() {
+  var carousel = document.addEventListener('postchange', function(event) {
+    console.log('Changed to ' + event.activeIndex)
+  });
 });
