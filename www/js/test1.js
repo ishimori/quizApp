50f7a41265821716730811205658c5d6f3440768
ishimori <ishimori_yuzuru@probsc.jp>
2017-08-18 11:59:40 +0900

    Dialogs

--- www/js/test1.js
+++ www/js/test1.js
@@ -1,15 +1,29 @@
-var prev = function() {
-  var carousel = document.getElementById('carousel');
-  carousel.prev();
+var showDialog = function (id) {
+  document
+    .getElementById(id)
+    .show();
 };
 
-var next = function() {
-  var carousel = document.getElementById('carousel');
-  carousel.next();
+var hideDialog = function (id) {
+  document
+    .getElementById(id)
+    .hide();
 };
 
-ons.ready(function() {
-  var carousel = document.addEventListener('postchange', function(event) {
-    console.log('Changed to ' + event.activeIndex)
-  });
-});
+var fromTemplate = function () {
+  var dialog = document.getElementById('dialog-3');
+
+  if (dialog) {
+    dialog.show();
+  }
+  else {
+    ons.createDialog('dialog.html')
+      .then(function (dialog) {
+        dialog.show();
+      });
+  }
+};
+
+function toggleToast() {
+  document.querySelector('ons-toast').toggle();
+}
