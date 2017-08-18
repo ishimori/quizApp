bbab58399a06235ab7b362613acdf01f540ee2e9
ishimori <ishimori_yuzuru@probsc.jp>
2017-08-18 12:45:42 +0900

    KitchenSink Example 一部動かないけど、なかなか良い。

--- www/js/test1.js
+++ www/js/test1.js
@@ -1,29 +1,33 @@
-var showDialog = function (id) {
-  document
-    .getElementById(id)
-    .show();
+window.fn = {};
+
+window.fn.toggleMenu = function () {
+  document.getElementById('appSplitter').right.toggle();
 };
 
-var hideDialog = function (id) {
-  document
-    .getElementById(id)
-    .hide();
+window.fn.loadView = function (index) {
+  document.getElementById('appTabbar').setActiveTab(index);
+  document.getElementById('sidemenu').close();
 };
 
-var fromTemplate = function () {
-  var dialog = document.getElementById('dialog-3');
+window.fn.loadLink = function (url) {
+  window.open(url, '_blank');
+};
 
-  if (dialog) {
-    dialog.show();
-  }
-  else {
-    ons.createDialog('dialog.html')
-      .then(function (dialog) {
-        dialog.show();
-      });
+window.fn.pushPage = function (page, anim) {
+  if (anim) {
+    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title }, animation: anim });
+  } else {
+    document.getElementById('appNavigator').pushPage(page.id, { data: { title: page.title } });
   }
 };
 
-function toggleToast() {
-  document.querySelector('ons-toast').toggle();
-}
+ons.ready(function () {
+  const sidemenu = document.getElementById('appSplitter');
+  ons.platform.isAndroid() ? sidemenu.right.setAttribute('animation', 'overlay') : sidemenu.right.setAttribute('animation', 'reveal');
+
+  document.querySelector('#tabbar-page').addEventListener('postchange', function(event) {
+    if (event.target.matches('#appTabbar')) {
+      event.currentTarget.querySelector('ons-toolbar .center').innerHTML = event.tabItem.getAttribute('label');
+    }
+  });
+});
