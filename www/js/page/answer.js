7eaf248aa6aa7ecd596f8ed4bae6dce0cd5305c0
ishimori <ishimori_yuzuru@probsc.jp>
2017-10-04 11:57:59 +0900

    モック：クイズ実施の流れを実装。 問題 ・一旦休憩ボタンの動きが変。（login.htmlの問題が解決すればOK) ・デザインイケてない。

--- www/js/page/answer.js
+++ www/js/page/answer.js
@@ -1,5 +1,5 @@
 Answer = {
-    start_page:function(page, nav){
+    show_start_page:function(page, nav){
         
         //alert(page.data.title + page.data.content + page.data.fileKey);
         page.querySelector('.title').innerHTML=page.data.title;
@@ -12,6 +12,26 @@ Answer = {
             alert('this is test.');
         }
         
+    },
+    start_quiz:function(){
+            let nav = document.querySelector('#nav');
+            //alert('start_quiz');
+            // 問題ページを表示
+            nav.pushPage('quiz1.html',{'animation':'none'});
+        
+    },
+    show_answer:function(pAns){
+            let nav = document.querySelector('#nav');
+            //alert('start_quiz');
+            // 解答ページの表示
+            nav.pushPage('quiz2.html',{'animation':'none'});
+        
+    },
+    break_quiz:function(){
+        alert('break_quiz');
+        let nav = document.querySelector('#nav');
+            //nav.pushPage('login.html',{'animation':'none'});
+            nav.resetToPage('login.html',{'animation':'none'});
     }
     
 }
